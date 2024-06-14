2e6a1d7ba17d6ce71321d12ae461f9d0e744e567
Ashish <ashishmax31@gmail.com>
2024-06-14 15:57:05 +0530

    add organisation service and cluster service
    Signed-off-by: Ashish <ashishmax31@gmail.com>

---
+++ pkg/services/cluster_service.go
@@ -0,0 +1,64 @@
+package services
+
+import (
+	"context"
+
+	"github.com/ashishmax31/soradev-api-server/pkg/db"
+	"github.com/ashishmax31/soradev-api-server/pkg/errors"
+	"github.com/ashishmax31/soradev-api-server/pkg/logger"
+	"github.com/ashishmax31/soradev-api-server/pkg/models"
+	"github.com/ashishmax31/soradev-api-server/pkg/stores"
+	"github.com/ashishmax31/soradev-api-server/pkg/stores/pgstore"
+)
+
+type ClusterService interface {
+	GetClusterForOrg(ctx context.Context, orgID int) (*models.Cluster, *errors.ServiceError)
+	GetDefaultCluster(ctx context.Context) (*models.Cluster, *errors.ServiceError)
+	Get(ctx context.Context, ID string) (*models.Cluster, *errors.ServiceError)
+}
+
+type clusterService struct {
+	clusterStore stores.ClusterStore
+	logger       logger.Logger
+}
+
+func NewClusterService(spec ClusterServiceSpec) ClusterService {
+	return &clusterService{
+		clusterStore: pgstore.NewClusterStore(pgstore.ClusterStoreSpec{
+			SessionFactory: spec.SessionFactory,
+		}),
+		logger: spec.Logger,
+	}
+}
+
+type ClusterServiceSpec struct {
+	SessionFactory db.SessionFactory
+	Logger         logger.Logger
+}
+
+func (s *clusterService) GetClusterForOrg(ctx context.Context, orgID int) (*models.Cluster, *errors.ServiceError) {
+	cluster, err := s.clusterStore.GetClusterForOrg(ctx, orgID)
+	if err != nil {
+		s.logger.Errorf("failed to get cluster for org: %v", err)
+		return nil, err
+	}
+	return cluster, nil
+}
+
+func (s *clusterService) GetDefaultCluster(ctx context.Context) (*models.Cluster, *errors.ServiceError) {
+	cluster, err := s.clusterStore.GetDefaultCluster(ctx)
+	if err != nil {
+		s.logger.Errorf("failed to get default cluster: %v", err)
+		return nil, err
+	}
+	return cluster, nil
+}
+
+func (s *clusterService) Get(ctx context.Context, ID string) (*models.Cluster, *errors.ServiceError) {
+	cluster, err := s.clusterStore.Get(ctx, ID)
+	if err != nil {
+		s.logger.Errorf("failed to get cluster: %v", err)
+		return nil, err
+	}
+	return cluster, nil
+}
---
+++ pkg/services/organisation_service.go
@@ -0,0 +1,74 @@
+package services
+
+import (
+	"context"
+
+	"github.com/ashishmax31/soradev-api-server/pkg/db"
+	"github.com/ashishmax31/soradev-api-server/pkg/errors"
+	"github.com/ashishmax31/soradev-api-server/pkg/logger"
+	"github.com/ashishmax31/soradev-api-server/pkg/models"
+	"github.com/ashishmax31/soradev-api-server/pkg/stores"
+	"github.com/ashishmax31/soradev-api-server/pkg/stores/pgstore"
+)
+
+type OrganisationService interface {
+	GetDefaultOrg(ctx context.Context) (*models.Organisation, *errors.ServiceError)
+	Create(ctx context.Context, spec *models.Organisation) (*models.Organisation, *errors.ServiceError)
+	Get(ctx context.Context, ID int) (*models.Organisation, *errors.ServiceError)
+	Delete(ctx context.Context, ID int) *errors.ServiceError
+}
+
+type organisationService struct {
+	organisationStore stores.OrganisationStore
+	logger            logger.Logger
+}
+
+func NewOrganisationService(spec OrganisationServiceSpec) OrganisationService {
+	return &organisationService{
+		organisationStore: pgstore.NewOrganisationStore(pgstore.OrganisationStoreSpec{
+			SessionFactory: spec.SessionFactory,
+		}),
+		logger: spec.Logger,
+	}
+}
+
+type OrganisationServiceSpec struct {
+	SessionFactory db.SessionFactory
+	Logger         logger.Logger
+}
+
+func (s *organisationService) GetDefaultOrg(ctx context.Context) (*models.Organisation, *errors.ServiceError) {
+	org, err := s.organisationStore.GetDefaultOrg(ctx)
+	if err != nil {
+		s.logger.Errorf("failed to get default organisation: %v", err)
+		return nil, err
+	}
+	return org, nil
+}
+
+func (s *organisationService) Create(ctx context.Context, spec *models.Organisation) (*models.Organisation, *errors.ServiceError) {
+	org, err := s.organisationStore.Create(ctx, spec)
+	if err != nil {
+		s.logger.Errorf("failed to create organisation: %v", err)
+		return nil, err
+	}
+	return org, nil
+}
+
+func (s *organisationService) Get(ctx context.Context, ID int) (*models.Organisation, *errors.ServiceError) {
+	org, err := s.organisationStore.Get(ctx, ID)
+	if err != nil {
+		s.logger.Errorf("failed to get organisation: %v", err)
+		return nil, err
+	}
+	return org, nil
+}
+
+func (s *organisationService) Delete(ctx context.Context, ID int) *errors.ServiceError {
+	err := s.organisationStore.Delete(ctx, ID)
+	if err != nil {
+		s.logger.Errorf("failed to delete organisation: %v", err)
+		return err
+	}
+	return nil
+}
